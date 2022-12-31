06b5e34b3b2b71351ef7939794edba3d2ce86b25
Usmansuraj <usmansuraj466@gmail.com>
2022-12-31 08:44:09 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doublType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+
+	return (0);
+}
