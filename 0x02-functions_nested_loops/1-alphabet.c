61ab442bd4e17bd061a904b6b3eab16657db9f97
Usmansuraj <usmansuraj466@gmail.com>
2023-01-01 04:25:43 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_Alphebet - print alphabet in lower case
+ *
+ */
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+
+}
